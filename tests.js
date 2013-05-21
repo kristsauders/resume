9407da1b7af9357f7414e1326888e0afd209040c
Krists Auders <kristsauders@gmail.com>
2013-05-21 14:25:27 -0600

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,16 +1,19 @@
 var exec = require('child_process').exec,
     assert = require('assert');
 
-assert.ok(true, 'Running tests...');
-
 exports['test resume app'] = function(assert, done) {
   exec('node app.js', function(error, stdout, stderr) {
         assert.equal(error, null, error);
         assert.notEqual(stdout, null, stdout);
         done();
     });
+    
     setTimeout(function(){
-      done();
+        exec('/usr/bin/wkhtmltopdf http://localhost:8084/resume.html public/Krists_Auders_Resume.pdf', function(error, stdout, stderr) {
+            assert.equal(error, null, error);
+            assert.notEqual(stdout, null, stdout);
+            done();
+        });
     }, 10000);
 }
 
