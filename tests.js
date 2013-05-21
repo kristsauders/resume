1535594171d1f6fd21d324be64e55e14361ae08f
Krists Auders <kristsauders@gmail.com>
2013-05-21 14:15:44 -0600

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,11 +1,13 @@
+var exec = require('child_process').exec;
+assert.ok(true, 'Running tests...');
+
 exports['test resume app'] = function(assert, done) {
-  require('child_process').exec('node app.js', function(error, stdout, stderr) {
+  exec('node app.js', function(error, stdout, stderr) {
         assert.equal(error, null, error);
         assert.notEqual(stdout, null, stdout);
         done();
     });
     setTimeout(function(){
-      console.log('Exiting');
       done();
     }, 10000);
 }
